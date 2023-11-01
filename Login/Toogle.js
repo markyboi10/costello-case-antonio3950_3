67ef2961aa6ac7e36ccb78d67b250ca6d08d4447
Mark Case <casem@merrimack.edu>
2023-10-31 22:10:41 -0400

    Email validity checks and field clearing

--- Login/Toogle.js
+++ Login/Toogle.js
@@ -14,3 +14,58 @@ document.addEventListener("DOMContentLoaded", function () {
         loginForm.style.display = "block";
     });
 });
+
+// Regex for email
+function validateEmail(email) {
+    var regex = /^\S+@\S+\.\S+$/;
+    return regex.test(email);
+}
+
+function checkEmail(inputId) {
+
+    const emailInput = document.getElementById(inputId);
+    var email = emailInput.value;
+    document.getElementById(inputId).style.border = "";
+
+    // Check email validity
+    if (email && !validateEmail(email)) { // not valid
+        document.getElementById(inputId).style.border = "2px solid red";
+        return false;
+    } else { // valid
+        return true;
+    }
+}
+
+// Check if sign up passwords are the same
+function checkPassword() {
+    const passOne = document.getElementById("passwordOne").value;
+    const passTwo = document.getElementById("passwordTwo").value;
+
+    if (passOne === passTwo) {
+        return true;
+    } else if (!passOne && !passTwo) {
+        return false;
+    } else {
+        document.getElementById("passwordOne").style.border = "2px solid red";
+        document.getElementById("passwordTwo").style.border = "2px solid red";
+        return false;
+    }
+}
+
+// Clear sign up fields and border on back btn click
+function clearSingUpFields() {
+    document.getElementById("signupEmail").value = "";
+    document.getElementById("passwordOne").value = "";
+    document.getElementById("passwordTwo").value = "";
+    document.getElementById("passwordOne").style.border = "";
+    document.getElementById("passwordTwo").style.border = "";
+    document.getElementById("signupEmail").style.border = "";
+}
+
+// Clear log in fields and border on sign up btn click
+function clearLoginFields() {
+    document.getElementById("emailAddress").style.border = "";
+    document.getElementById("LoginPassword").style.border = "";
+    document.getElementById("emailAddress").value = "";
+    document.getElementById("LoginPassword").value = "";
+}
