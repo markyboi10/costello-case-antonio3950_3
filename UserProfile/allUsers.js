22811891f395622d8beb85886b86b3480c033269
Samara Ribeiro Antonio <ribeiroantos@merrimack.edu>
2023-11-13 18:11:48 -0500

    Fixes for all users to show just the username and not the whole email, fixes for when chosing a different video to play for the description of the previous video disapear, added more style, fixes for when the user comments on someone's else page for it to show the name of the user commenting.

--- UserProfile/allUsers.js
+++ UserProfile/allUsers.js
@@ -50,43 +50,42 @@ function addToFriendsList(friendUsername) {
 }
 
 function decodeAllUsers() {
-    fetch(`/getAllUsers`)
-        .then(response => response.json())
-        .then(data => {
-
-            // If endpoint response is incorrect
-            if (data.error) {
-                console.log(data.error);
-            } else {
-                /* 
-                 * Otherwise, set the data to local storage so it can be accessed 
-                 * by UserProfile where it is navigating to
-                */
-                console.log("The data: " + data)
-                updateUI(data);
-            }
-        })
-        .catch(error => {
-            console.error('User failed to be added', error);
-        });
+  fetch(`/getAllUsers`)
+    .then((response) => response.json())
+    .then((data) => {
+      // If endpoint response is incorrect
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        /*
+         * Otherwise, set the data to local storage so it can be accessed
+         * by UserProfile where it is navigating to
+         */
+        console.log("The data: " + data);
+        updateUI(data);
+      }
+    })
+    .catch((error) => {
+      console.error("User failed to be added", error);
+    });
 }
 
 // Update ALL USERS code using sse and normal endpoint
 
 // Open an SSE connection to the server ednpoint
-const eventSource = new EventSource('/sse-routine-new-users');
+const eventSource = new EventSource("/sse-routine-new-users");
 
 eventSource.onmessage = handleSSE;
 
 // Function to handle SSE messages
 function handleSSE(event) {
-    const newData = JSON.parse(event.data); // data from endpoint
-    updateUI(newData); // Call update UI with the new data
+  const newData = JSON.parse(event.data); // data from endpoint
+  updateUI(newData); // Call update UI with the new data
 }
 
 eventSource.onerror = (error) => {
-    // Handle errors such as lost connection
-    console.error('SSE Error', error);
+  // Handle errors such as lost connection
+  console.error("SSE Error", error);
 };
 
 // Grab the id of the dropdown menu for all users
@@ -94,15 +93,15 @@ const dropdownMenu = document.getElementById("all_users_dd");
 
 // Update the index html with the newData as input
 function updateUI(newData) {
-    dropdownMenu.innerHTML = ""; // Clear list so it is not beind duplicated
-    newData.forEach(function (email) {
-        const listItem = document.createElement("li"); // Create a new list item
-        listItem.addEventListener("click", () => visitOtherUser(email));
-        const anchor = document.createElement("a"); // Create an anchor element
-        anchor.textContent = email; // Set the text content of the anchor to the email
-        anchor.href = "#"; // Set the href attribute as needed
+  dropdownMenu.innerHTML = ""; // Clear list so it is not being duplicated
+  newData.forEach(function (email) {
+    const listItem = document.createElement("li"); // Create a new list item
+    listItem.addEventListener("click", () => visitOtherUser(email));
+    const anchor = document.createElement("a"); // Create an anchor element
+    anchor.textContent = email.split("@")[0]; // Set the text content of the anchor to the email
+    anchor.href = "#"; // Set the href attribute as needed
 
-        listItem.appendChild(anchor); // Append the anchor to the list item
-        dropdownMenu.appendChild(listItem); // Append the list item to the dropdown menu
-    });
+    listItem.appendChild(anchor); // Append the anchor to the list item
+    dropdownMenu.appendChild(listItem); // Append the list item to the dropdown menu
+  });
 }
